419f52e81b5a392ea3caa01906a43e73be1d45c5
ankushmalode <you@example.com>
2021-09-29 19:13:33 +0530

    remove unnecessary line & check indentation

---
+++ src/main/java/com/bridgelabz/Quadratic.java
@@ -0,0 +1,24 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class Quadratic {
+    public static void main(String[] args) {
+        Scanner value=new Scanner(System.in);
+
+    }
+}
+//public class QuadraticRoots {
+//    public static void main(String[] args){
+//        double a=(Math.random() * 10);
+//        double b=(Math.random() * 10);
+//        double c=(Math.random() * 10);
+//        double d = Math.sqrt(b*b - 4* a *c);
+//
+//        double root1 = -b + d/2*a;
+//        double root2 = -b - d/2*a;
+//        System.out.println(root1);
+//        System.out.println(root2);
+//    }
+//
+//}
--- src/main/java/com/bridgelabz/WindChill.java
+++ src/main/java/com/bridgelabz/WindChill.java
@@ -3,23 +3,24 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class WindChill {
-    static double Wind(double t, double v){
-        double w=0;
-        if (t>50 && v<120 && v>3){
-           w =(double) 35.74+(0.6215*t)+((0.4275*t)-35.75)*Math.pow(v,0.16);
-        }else
+    static double Wind(double t, double v) {
+        double w = 0;
+        if (t > 50 && v < 120 && v > 3) {
+            w = 35.74 + (0.6215 * t) + ((0.4275 * t) - 35.75) * Math.pow(v, 0.16);
+        } else
             System.out.println("You have entered wrong values.");
         return w;
     }
+
     public static void main(String[] args) {
-        Scanner value= new Scanner(System.in);
+        Scanner value = new Scanner(System.in);
         System.out.println("You have to enter value Temperature>50 & 3<speed<120 in between.");
         System.out.print("Enter Temperature t(in Fahrenheit): ");
-        double temp=value.nextDouble();
+        double temp = value.nextDouble();
         System.out.print("Enter wind speed v: ");
-        double speed= value.nextDouble();
+        double speed = value.nextDouble();
 
-        double c=Wind(temp,speed);
+        double c = Wind(temp, speed);
         System.out.println(c);
     }
 }
